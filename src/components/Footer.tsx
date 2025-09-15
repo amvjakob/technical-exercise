a9471609e95d594416d0469ff4e3f62f2daaa58c
Anthony Jakob <anthony.jakob@outlook.com>
2025-09-15 14:39:51 +0200

    :hammer: Adapt footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div className="w-full px-5 py-5 xl:m-0 mt-5 flex justify-between gap-2 font-semibold xl:text-sm">
       <span className="hidden xl:inline-flex text-sm">
-        React Dashboard
+        Dashboard
       </span>
       <div className="flex gap-1 items-center">
         <span className="text-sm">Dashboard</span>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,5 @@
 // import React from 'react';
 import ChangeThemes from '../components/ChangesThemes';
-import { DiReact } from 'react-icons/di';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
@@ -16,9 +15,8 @@ const Login = () => {
         </div>
         <div className="w-full h-screen xl:h-auto xl:w-[30%] 2xl:w-[25%] 3xl:w-[20%] bg-base-100 rounded-lg shadow-md flex flex-col items-center p-5 pb-7 gap-8 pt-20 xl:pt-7">
           <div className="flex items-center gap-1 xl:gap-2">
-            <DiReact className="text-4xl sm:text-4xl xl:text-6xl 2xl:text-6xl text-primary animate-spin-slow -ml-3" />
             <span className="text-[18px] leading-[1.2] sm:text-lg xl:text-3xl 2xl:text-3xl font-semibold text-base-content dark:text-neutral-200">
-              React Dashboard
+              Dashboard
             </span>
           </div>
           <span className="xl:text-xl font-semibold">
